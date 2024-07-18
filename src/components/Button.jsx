3e3d9a9f96227268f4b22f13136c994464403d31
Fedor_Semochkin <221800ss@gmail.com>
2024-07-18 19:01:22 +0700

    adding offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="flex bg-coral-red border-coral-red rounded-full border font-montserrat text-white text-lg px-7 py-4 gap-2 leading-none justify-center items-center">
-        {label}
-        {iconURL && 
+    <button className={`flex ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red border-coral-red text-white'} rounded-full border font-montserrat text-lg px-7 py-4 gap-2 leading-none justify-center items-center`}>
+      {label}
+      {iconURL && 
         <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full h-5 w-5" />
-        }
+      }
     </button>
   )
 }
 
-export default Button
+export default Button;
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,34 @@
-
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="" width={773} height={687} className="w-full object-contain" />
+        
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-bold text-4xl font-palanquin capitalize lg:max-w-lg">
+          <br />
+          <span className="text-coral-red inline-block mt-3">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis facere rem quam neque? Excepturi ipsam suscipit deserunt perspiciatis accusamus similique! Facere consequatur amet architecto blanditiis quia perspiciatis tempore recusandae placeat!
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor incidunt repellat velit tenetur magni minima debitis inventore mollitia dolores unde eligendi cumque veritatis.</p>
+        <div className="mt-11 flex flex-wrap gap-4">
+        <Button label='Wiew details'  />
+        <Button label='Learn more' 
+        backgroundColor = 'bg-white' 
+        borderColor = 'border-slate-gray'
+        textColor = 'text-slate-gray'
+        />
+
+        </div>
+      </div>
+    </section>
   )
 }
 
